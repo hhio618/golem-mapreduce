468bd0c877f4ecc8040f2df73f2b3105168bd5ae
ox26a <hhio618@gmail.com>
2021-06-03 03:02:18 +0430

    add map reducer

--- mapreduce/context.py
+++ mapreduce/context.py
@@ -1,4 +1,7 @@
 from functools import reduce
+from itertools import groupby
+from collections import defaultdict
+
 
 class GolemMapReduceContext:
     def __init__(self, current):
@@ -10,6 +13,9 @@ class GolemMapReduceContext:
 
     def set_current(self, current):
         self.current
+
+    def flatMap(self, mapper):
+        self.current = [y for y in mapper(x) for x in self.current]
     
     def map(self, mapper):
         map(mapper, self.current)
@@ -22,6 +28,21 @@ class GolemMapReduceContext:
 
     def reduceByKey(self, reducer):
         reduce(reducer, self.current)
+
+    def groupByKey(self):
+        v = defaultdict(list)
+        for key, value in sorted(self.current.items()):
+            v[value].append(key)
+        self.current = v.items()
+
+    def distinct(self):
+        self.current = list(set(self.current))
+
+    def filter(self, filterer):
+        filter(filterer, self.current)
+
+    def count(self):
+        return len(self.current)
 
     def cache(self):
         self.cache = current.copy()
